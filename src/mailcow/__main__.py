2e54d721e5c97cb172a4ce415153c1e027e4081a
Jean-Denis Gebhardt <projects@der-jd.de>
2021-02-05 15:32:41 +0100

    docs: Add missing license string

--- src/mailcow/__main__.py
+++ src/mailcow/__main__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# Copyright (c) 2021 Jean-Denis Gebhardt <projects@der-jd.de>
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 '''Main'''
 
 import mailcow.cli
